aa1ed614b9ffce3218d970400398b385a1ec4b45
MARIABODOWSKAYA\Мария Бодовская <mashabodovskaya@gmail.com>
2021-10-08 01:18:47 +0200

    configuring views and urls for the store app writing paths to the listed pages

---
+++ ecommerce/store/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.shop, name="shop"),
+    path('cart/', views.cart, name="cart"),
+    path('payment/', views.payment, name="payment"),
+    path('about/', views.about, name="about"),
+]
