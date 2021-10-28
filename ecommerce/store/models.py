cc8b7598cd795debafad6286fddd3bc69bd86a3d
royal_T_baby <mashabodovskaya@gmail.com>
2021-10-28 04:15:08 +0200

    Writing the shipping form logic. Making sure we do have the certain items available for shipping. Adding the hidden logic to the Continue and Pay buttons on the Payment page

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -19,6 +19,7 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    digital = models.BooleanField(default=True, null=True, blank=False)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
@@ -41,6 +42,15 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for _ in orderitems:
+            if _.product.digital == False:
+                shipping = True
+        return shipping
 
     @property
     def get_cart_total(self):
